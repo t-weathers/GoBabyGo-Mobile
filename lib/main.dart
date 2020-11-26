2d837e3e7797899d44fb1a55d59418b2c7816078
t-weathers <38232272+t-weathers@users.noreply.github.com>
2020-11-25 16:03:07 -0800

    first attempt at login screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 //From get data from internet
 
 Future<Album> fetchAlbum() async {
-  final response =
-  await http.get('http://127.0.0.1:5000/');
+  final response = await http.get('http://127.0.0.1:5000/');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -40,7 +38,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -86,62 +83,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<Album> album;
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    album = fetchAlbum();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TextStyle style =
+      TextStyle(fontFamily: 'Montserrat', color: Colors.white, fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final emailField = TextField(
+        obscureText: false,
+        style: style,
+        cursorColor: Colors.white,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          hintStyle: TextStyle(color: Colors.white),
+          filled: true,
+          fillColor: Color.fromRGBO(235, 152, 78, 1.0),
+          //border before being clicked
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+          //border after click
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+        ));
+    final passwordField = TextField(
+        obscureText: true,
+        style: style,
+        cursorColor: Colors.white,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          hintStyle: TextStyle(color: Colors.white),
+          filled: true,
+          fillColor: Color.fromRGBO(235, 152, 78, 1.0),
+          //border on load (not clicked)
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+          //border when clicked
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.transparent),
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+        ));
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.orange[800],
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        //THIS IS WHERE THE BUTTON CLICK GOES
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: FutureBuilder<Album>(
-          future: album,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text(snapshot.data.data);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+    );
 
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.orange[800], Colors.white])),
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 200.0,
+                  child: Image.asset(
+                    "assets/GoBabyGo_WEBLOGO.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter(),
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      //  ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
