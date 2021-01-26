e5559ed41ef2cf62a2d07b1be3c5dc81de634361
Thomas Weathers <thomasweathers@Thomass-MacBook-Pro.local>
2021-01-26 13:43:56 -0800

    refactor main to home

--- lib/home.dart
+++ lib/home.dart
@@ -8,47 +8,12 @@ import 'package:testing_app/timelog.dart';
 import 'package:testing_app/forum.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
-import './my_flutter_app_icons.dart';
+import 'my_flutter_app_icons.dart';
 
 
 //From get data from internet
 
 
-
-
-
-//// END /////
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Go Baby Go - About',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        //primarySwatch: Colors.orange,
-        primaryColor: Colors.orange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Go Baby Go'),
-    );
-  }
-}
 
 //first home page on login
 
@@ -90,9 +55,6 @@ class _MyHomePageState extends State<MyHomePage> {
     timelog(),
     activities(),
     faq(),
-
-
-
   ];
 
   void onTappedBar(int index){
