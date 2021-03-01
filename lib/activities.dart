7aed75c94cd2284a4631dee37e0d5bc3918af2f6
vandomni <43551200+vandomni@users.noreply.github.com>
2021-02-28 18:23:09 -0800

    Grid View displaying categories
    GridView now correctly displaying categories from firebase and print statements for the activity names. Currently using CategoriesTest for the database reference

--- lib/activities.dart
+++ lib/activities.dart
@@ -3,22 +3,23 @@ import 'dart:convert';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dialog.dart';
 
-// class Category {
-//   final int id;
-//   final String categoryName;
-//   final List<Activity> activityNames;
-//
-//   Category({this.id, this.categoryName, this.activityNames});
-//
-//   factory Category.fromJson(Map<String, dynamic> json){
-//     return Category(
-//         id: json[],
-//         categoryName: json["CategoryName"],
-//         activityNames: json[]
-//     );
-//   }
-// }
+
+class Category {
+  //final int id;
+  final String categoryName;
+  final List<String> activityNames;
+
+  Category({this.categoryName, this.activityNames});
+
+  factory Category.fromJson(Map<String, dynamic> json){
+    return Category(
+        categoryName: json["CategoryName"],
+        activityNames: json["Activities"]
+    );
+  }
+}
 //
 // class Activity {
 //   final int id;
@@ -35,9 +36,11 @@ class activities extends StatefulWidget{
 }
 
 class _activitiesState extends State<activities> {
-  final dbRef = FirebaseDatabase.instance.reference().child("Categories");
+  final dbRef = FirebaseDatabase.instance.reference().child("Categories_test");
+  final dbRefAct = FirebaseDatabase.instance.reference().child("Categories_test/Activities");
   List<Map<dynamic, dynamic>> lists = [];
-  final catArray = ["Driving"];
+  final catArray = ["Zero", "One"];
+  final actArray = ["Activity0", "Activity1"];
 
   @override
   Widget build(BuildContext context) {
@@ -61,11 +64,27 @@ class _activitiesState extends State<activities> {
                itemCount: lists.length,
                itemBuilder: (BuildContext context, int index){
                  return Container(
+                   child: Material(
+                     child: InkWell(
+                     onTap: (){
+                       int actIndex = index;
+                       String activityArray;
+                       while(actArray.length != 0) {
+                         print(lists[index][catArray[index]]["Activity" +
+                             actIndex.toString()])  ;
+                         actIndex++;
+                         actArray.length--;
+                         //print(activityArray);
+                       }
+                       print(lists[index][catArray[index]]["categoryName"]);
+                     },
                    child: Card(
                      child: Center(
                       child: Text(lists[index][catArray[index]]["categoryName"], style: TextStyle(fontSize:18, color:Colors.grey[600]))
                      )
                    )
+                 )
+                 )
                  );
                });
             }
