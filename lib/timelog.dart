0962ae42e2621be9d20ffe0ab2461c2b917d8124
eviemayy <43556792+eviemayy@users.noreply.github.com>
2021-04-23 13:16:42 -0700

    timelog page finished

--- lib/timelog.dart
+++ lib/timelog.dart
@@ -18,6 +18,34 @@ String formatTime(int milliseconds){
   return "$hours:$minutes:$seconds";
 }
 
+bool isDateInRange(String date){
+  //Sunday - Saturday == each week range
+  //print("INSIDE ISDATEINRANGE()");
+  var parsedDate = DateTime.parse(date);
+  //print("DATE: " + parsedDate.toString());
+
+  DateTime now = DateTime.now();
+  int currentDay = now.weekday;
+  DateTime firstDayOfWeek = now.subtract(Duration(days: currentDay));
+  DateTime lastDayOfWeek = firstDayOfWeek.add(Duration(days: 6));
+  //print("first day of week: "+ firstDayOfWeek.toString());
+  //print("last day of week: "+ lastDayOfWeek.toString());
+  return parsedDate.isAfter(firstDayOfWeek) && parsedDate.isBefore(lastDayOfWeek);
+}
+
+int calculateTimeThisWeek(List weeklyRecordedTime){
+  //DateTime total = 0;
+  var prefix = '0000-01-01T';
+  var elapsedMin = 0;
+  //  loop through each time log
+  weeklyRecordedTime.forEach((time) {
+    var parsedDate = DateTime.parse(prefix + time.toString());  //turn string into datetime
+    elapsedMin = elapsedMin + parsedDate.minute + (parsedDate.hour * 60); //add hours and min to elapsed min
+  });
+  //print("elapsed min: " + elapsedMin.toString());
+  return elapsedMin;
+}
+
 class timelog extends StatefulWidget{
   timelog({Key key, this.gsi, this.signIn, this.parent, this.child}) : super(key: key);
   //final String gsi;
@@ -34,10 +62,13 @@ class timelog extends StatefulWidget{
 class _timelogState extends State<timelog>{
 
   final dbRef = FirebaseDatabase.instance.reference().child("ParentUsers");
+  final timelogRef = FirebaseDatabase.instance.reference().child("TimeLogs");
   String entryKey;
   String childName, parentName, email;
-  int weeklyGoal;
+  int weeklyGoal, totalRecordedTime;
   List<Map<dynamic, dynamic>> userInfo = [];
+  List<Map<dynamic, dynamic>> timelogInfo = [];
+  List weeklyRecordedTime = [];
 
   //Initialize timer variables
   Stopwatch _stopwatch;
@@ -52,36 +83,7 @@ class _timelogState extends State<timelog>{
   void initState() {
     super.initState();
 
-    //FROM PROFILE.DART
-    Future <void> populateInfo() async {
-
-      //it warns to "indexOn": "Email" here. Maybe look into that?
-      DataSnapshot data = await dbRef.orderByChild('Email').equalTo(
-          widget.gsi.email).once();
-
-      Map<dynamic, dynamic> values = data.value;
-
-      userInfo.clear();
-      values.forEach((key, value){
-        userInfo.add(values);
-        entryKey = key; //this is the user ID
-
-      });
-      //print("This is the list of all users: ???");
-      //print(userInfo);
-
-      childName = userInfo[0][entryKey]['ChildFirstName'];
-      parentName = userInfo[0][entryKey]['FirstName'];
-      weeklyGoal = userInfo[0][entryKey]['WeeklyGoal'];
-      //email = userInfo[0][entryKey]['Email'];
-      //print("parent name: " + parentName);
-      //print("child name: " + childName);
-      //print("weekly goal: " + weeklyGoal.toString());
-      //print("email: " + email);
-      print("entry key: " + entryKey);
-    }
     populateInfo();
-
     _stopwatch = Stopwatch();
     //re render every 30ms
     _timer = new Timer.periodic(new Duration(milliseconds: 30), (timer) {
@@ -89,11 +91,54 @@ class _timelogState extends State<timelog>{
     });
 
     //every ten min this page will update info from the database. To often? or good? idk...
-    _updatetimer = new Timer.periodic(new Duration(minutes: 10), (updatetimer) {
-      print("printing");
-      populateInfo();
-    });
+    // _updatetimer = new Timer.periodic(new Duration(minutes: 10), (updatetimer) {
+    //   print("printing");
+    //   populateInfo();
+    // });
 
+  }
+
+  //FROM PROFILE.DART
+  Future <void> populateInfo() async {
+
+    //it warns to "indexOn": "Email" here. Maybe look into that?
+    DataSnapshot data = await dbRef.orderByChild('Email').equalTo(
+        widget.gsi.email).once();
+
+    Map<dynamic, dynamic> values = data.value;
+
+    userInfo.clear();
+    values.forEach((key, value){
+      userInfo.add(values);
+      entryKey = key; //this is the user ID
+
+    });
+    //print("This is the list of all users: ???");
+    //print(userInfo);
+
+    childName = userInfo[0][entryKey]['ChildFirstName'];
+    parentName = userInfo[0][entryKey]['FirstName'];
+    weeklyGoal = userInfo[0][entryKey]['WeeklyGoal'];
+    print("userID: " + entryKey);
+
+    // Now we get all the time log associated with that user
+    DataSnapshot timelogData = await timelogRef.orderByChild('UserID').equalTo(entryKey).once();
+    Map<dynamic, dynamic> timelogvalues = timelogData.value;
+
+    timelogInfo.clear();
+    weeklyRecordedTime.clear();
+    timelogvalues.forEach((key, value){
+      timelogInfo.add(timelogvalues);
+      //print("looping, values = " + timelogvalues.toString());
+      //print("timelogInfo[0][key]['TotalTime']: "+ timelogInfo[0][key]['TotalTime']);
+      //print("timelogInfo[0][key]['StartTime']: "+ timelogInfo[0][key]['StartTime']);
+      var inWeeklyRange = isDateInRange(timelogInfo[0][key]['StartTime']);
+      if(inWeeklyRange){
+        //print("this date is within this week");
+        weeklyRecordedTime.add(timelogInfo[0][key]['TotalTime']);
+      }
+    });
+    totalRecordedTime = calculateTimeThisWeek(weeklyRecordedTime);
   }
 
   @override
@@ -112,14 +157,13 @@ class _timelogState extends State<timelog>{
       _stopwatch.start();
       if(_stopwatch.elapsedMilliseconds == 0){
         startTime = new DateTime.now();
-        print("start time: " + startTime.toString());
+        //print("start time: " + startTime.toString());
       }
     }
     setState(() { }); //re render the page
   }
 
   Future _handleTimeSave() async {
-
     //DateTime now = new DateTime.now();
     //DateTime date = new DateTime(now.year, now.month, now.day);
 
@@ -130,9 +174,6 @@ class _timelogState extends State<timelog>{
 
     await showDialog(
         context: context,
-        /*it shows a popup with few options which you can select, for option we
-        created enums which we can use with switch statement, in this first switch
-        will wait for the user to select the option which it can use with switch cases*/
         child: new SimpleDialog(
           title: ListTile(
               title: const Text("TIME SAVED", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25)),
@@ -169,11 +210,11 @@ class _timelogState extends State<timelog>{
       'TotalTime': formatTime(_stopwatch.elapsedMilliseconds).toString(),
       'UserID': entryKey.toString()
     };
-    print("timeLogData: " + timeLogData.toString());
+    //print("timeLogData: " + timeLogData.toString());
 
     //push new time log entry
     dbRef.push().set(timeLogData);
-
+    populateInfo();
     _stopwatch.reset();
 
   }
@@ -223,7 +264,7 @@ class _timelogState extends State<timelog>{
                 ),
                 FlatButton(onPressed: () {
                     //navigate to new page here
-                    print("manual entry pressed");
+                    //print("manual entry pressed");
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => timelogManualEntry()),
@@ -231,8 +272,8 @@ class _timelogState extends State<timelog>{
                   },
                     child: Text("Manual Entry", style: TextStyle(decoration: TextDecoration.underline))),
                 SizedBox(height: 50),
-                _progressText(weeklyGoal),
-                _progresssBar(),
+                _progressText(weeklyGoal, totalRecordedTime),
+                _progresssBar(weeklyGoal, totalRecordedTime),
                 _goalButtons(context, user)
               ]
           ),
@@ -242,7 +283,7 @@ class _timelogState extends State<timelog>{
   }
 }
 
-Widget _progressText(int weeklyGoal){
+Widget _progressText(int weeklyGoal, int totalRecordedTime){
   var notSet = false;
   if(weeklyGoal == 0 || weeklyGoal == null){
     notSet = true;
@@ -254,34 +295,42 @@ Widget _progressText(int weeklyGoal){
       child: Row(
         children: <Widget>[
           Expanded(child: Text(
-            'Weekly Goal: ' + (notSet ? '(Not Set)' : weeklyGoal.toString()),
-            style: TextStyle(fontWeight: FontWeight.bold),
+            'Weekly Goal: ' + (notSet ? '(Not Set)' : weeklyGoal.toString() + ' min'),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
           )),
-          Spacer(),
-          Expanded(child: Text(
-            'Progress: 30 min',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ))
+          Expanded(child: Padding(
+              padding: EdgeInsets.fromLTRB(60, 0, 10, 0),
+              child: Text('Progress: ' + totalRecordedTime.toString() + ' min',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+          )))
         ],
       ),
     ),
   );
 }
 
-Widget _progresssBar(){
+Widget _progresssBar(int weeklyGoal, int totalRecordedTime){
+
+  double ratio = 0.0;
+  double percent = 0.0;
+
+  if(weeklyGoal != null && totalRecordedTime !=null){
+    ratio = totalRecordedTime/weeklyGoal;
+    percent = ratio * 100;
+  }
   return Container(
     padding: const EdgeInsets.all(20.0),
     child: Center(
       child: Row(
         children: <Widget>[
           Expanded(child: LinearProgressIndicator(
-              value: 0.3, //this will be pulled from users progress
+              value: ratio, //this will be pulled from users progress
               semanticsLabel: 'Progress towards goal',
               minHeight: 30
           ),),
           Padding(
             padding: EdgeInsets.all(10.0),
-            child: Text('30%'),
+            child: Text(double.parse((percent).toStringAsFixed(2)).toString()+"%"),
           ),
 
         ],
@@ -303,7 +352,7 @@ Widget _goalButtons(BuildContext context, final user) {
         child: new Text("EDIT GOAL"),
         onPressed: () {
           //navigate to new page here
-          print("edit goal pressed");
+          //print("edit goal pressed");
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -317,7 +366,7 @@ Widget _goalButtons(BuildContext context, final user) {
         child: new Text("WEEKLY PROGRESS"),
         onPressed: () {
           //navigate to new page here
-          print("weekly progress pressed");
+          //print("weekly progress pressed");
           Navigator.push(context, MaterialPageRoute(builder: (context) => weeklyProgress()),
           );
         },
